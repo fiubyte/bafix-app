192971bc90cb3abba20e0e9413a5b45da559e66e
Carolina Pistillo <cpistillo@fi.uba.ar>
2024-04-10 00:14:43 -0300

    fiubyte/.github#9: Add ServiceFinder fragment and basic UI

--- app/src/main/java/com/fiubyte/bafix/RegisterFragment.java
+++ app/src/main/java/com/fiubyte/bafix/RegisterFragment.java
@@ -6,10 +6,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class RegisterFragment extends Fragment {
 
@@ -33,6 +35,12 @@ public class RegisterFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
+        ImageView googleSignInButton = view.findViewById(R.id.google_sign_in_button);
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_registerFragment_to_serviceFinderFragment);
+            }
+        });
     }
 }
