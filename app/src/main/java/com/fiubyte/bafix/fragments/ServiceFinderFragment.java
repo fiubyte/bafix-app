1f28e324a1730384c4d43393aa0244b5b5ce9999
Carolina Pistillo <cpistillo@fi.uba.ar>
2024-04-12 14:20:19 -0300

    fiubyte/.github#9 Prototype query with OkHttpClient

--- app/src/main/java/com/fiubyte/bafix/fragments/ServiceFinderFragment.java
+++ app/src/main/java/com/fiubyte/bafix/fragments/ServiceFinderFragment.java
@@ -1,6 +1,7 @@
 package com.fiubyte.bafix.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,16 +16,27 @@ import com.fiubyte.bafix.R;
 import com.fiubyte.bafix.adapters.ServiceFinderListAdapter;
 import com.fiubyte.bafix.entities.ProviderData;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ServiceFinderFragment extends Fragment {
 
     private final ArrayList<ProviderData> providers = new ArrayList<>();
+    private OkHttpClient client;
+    private String getServicesURL = "https://bafix-api.onrender.com/service-categories/";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        client = new OkHttpClient();
 
         setupProviders();
     }
@@ -49,6 +61,25 @@ public class ServiceFinderFragment extends Fragment {
     }
 
     private void setupProviders() {
+        Request request = new Request.Builder()
+                                .url(getServicesURL)
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTI4OTkxNDgsImlhdCI6MTcxMjg3MDM0OCwic3ViIjoiYWRtaW5AZXhhbXBsZS5jb20iLCJyb2xlcyI6IkFETUlOIn0.AEgqdirUrtgCIrAX1N2VxdyCM2pOOYfzpCoLmJ2WfKc")
+                                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.e("DEBUGGING", e.toString());
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                Log.d("DEBUGGING", response.body().string());
+            }
+        });
+
+
         providers.add(
                 new ProviderData("Julia Benavidez",
                                 0.8,
@@ -76,7 +107,7 @@ public class ServiceFinderFragment extends Fragment {
         providers.add(
                 new ProviderData("Carla Pasaft",
                         2.5,
-                        new ArrayList<>(Arrays.asList("Electricista", "Cerrajeria", "Reparacion de electrodomesticos")),
+                        new ArrayList<>(Arrays.asList("Decoracion de interiores")),
                         R.drawable.carla));
     }
 }
