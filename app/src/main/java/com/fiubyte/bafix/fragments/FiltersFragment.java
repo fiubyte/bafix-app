4889fbf4a72079289dc819f96dcc7f5916a80f32
Carolina Pistillo <cpistillo@fi.uba.ar>
2024-04-14 04:26:46 -0300

    fiubyte/.github#12 Add UI behaviour when buttons are selected

--- app/src/main/java/com/fiubyte/bafix/fragments/FiltersFragment.java
+++ app/src/main/java/com/fiubyte/bafix/fragments/FiltersFragment.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.fiubyte.bafix.R;
+import com.google.android.material.card.MaterialCardView;
 
 public class FiltersFragment extends Fragment {
 
+    MaterialCardView availableNowButton;
+    MaterialCardView applyButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +29,32 @@ public class FiltersFragment extends Fragment {
             Bundle savedInstanceState
                             ) {
         return inflater.inflate(R.layout.fragment_filters, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        availableNowButton = view.findViewById(R.id.available_now_button);
+        applyButton = view.findViewById(R.id.apply_button);
+
+        availableNowButton.setOnClickListener(v -> {
+            handleAvailableButtonClicked();
+        });
+
+        applyButton.setOnClickListener(v -> {
+            handleApplyButtonClicked(v);
+        });
+    }
+
+    private void handleAvailableButtonClicked() {
+        availableNowButton.setStrokeColor(getResources().getColor(R.color.selected_color));
+        ((TextView)availableNowButton.getChildAt(0)).setTextColor(getResources().getColor(R.color.selected_color));
+    }
+
+    private void handleApplyButtonClicked(View view) {
+        Navigation
+                .findNavController(view)
+                .navigate(R.id.action_filtersFragment_to_serviceFinderFragment);
     }
 }
