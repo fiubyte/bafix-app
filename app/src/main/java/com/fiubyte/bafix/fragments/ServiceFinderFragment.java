eb4bbd3b67c617f5fd03d553a6665967c39cb15f
Carolina Pistillo <cpistillo@fi.uba.ar>
2024-04-24 20:10:52 -0300

    fiubyte/.github#15 Update and modularize way of handling views visibility on services fragment

--- app/src/main/java/com/fiubyte/bafix/fragments/ServiceFinderFragment.java
+++ app/src/main/java/com/fiubyte/bafix/fragments/ServiceFinderFragment.java
@@ -20,12 +20,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.fiubyte.bafix.R;
 import com.fiubyte.bafix.adapters.ServiceFinderListAdapter;
 import com.fiubyte.bafix.models.DataViewModel;
-import com.fiubyte.bafix.models.FiltersViewModel;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServiceFinderFragment extends Fragment implements View.OnClickListener {
+    enum ServicesView {
+        LIST,
+        MAP,
+        NO_SERVICES,
+        BACKEND_ERROR
+    }
 
+    private ServicesView currentView = ServicesView.LIST;
+    private Map<ServicesView, View> views;
     private DataViewModel dataViewModel;
-    private FiltersViewModel filtersViewModel;
     private RecyclerView recyclerView;
 
     private ImageView filterButton;
@@ -52,45 +61,74 @@ public class ServiceFinderFragment extends Fragment implements View.OnClickListe
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        initializeViews(view);
+        setupViewModels();
+        initializeListView();
+        updateServicesView();
+    }
+
+    private void initializeViews(View view) {
         recyclerView = view.findViewById(R.id.serviceFinderRecyclerView);
-        dataViewModel = new ViewModelProvider(requireActivity()).get(DataViewModel.class);
-        filtersViewModel = new ViewModelProvider(requireActivity()).get(FiltersViewModel.class);
         noServicesOffered = view.findViewById(R.id.no_services_offered);
         noServicesAvailable = view.findViewById(R.id.services_not_available_cardview);
+        filterButton = view.findViewById(R.id.filters_button);
+        mapsButton = view.findViewById(R.id.maps_button);
 
+        views = new HashMap<>();
+        views.put(ServicesView.LIST, recyclerView);
+        views.put(ServicesView.NO_SERVICES, noServicesOffered);
+        views.put(ServicesView.BACKEND_ERROR, noServicesAvailable);
+
+        filterButton.setOnClickListener(this);
+        mapsButton.setOnClickListener(this);
+    }
+
+    private void setupViewModels() {
+        dataViewModel = new ViewModelProvider(requireActivity()).get(DataViewModel.class);
+    }
+
+    private void updateServicesView() {
+        updateCurrentView();
+
+        views.forEach((viewType, view) -> {
+            if (viewType == currentView) {
+                view.setVisibility(View.VISIBLE);
+            } else {
+                view.setVisibility(View.GONE);
+            }
+        });
+
+        updateViewSwitcherButtons();
+    }
+
+    private void updateViewSwitcherButtons() {
+         if (currentView == ServicesView.LIST) {
+            mapsButton.setVisibility(View.VISIBLE);
+         } else if (currentView == ServicesView.MAP) {
+             mapsButton.setVisibility(View.GONE);
+         } else {
+             mapsButton.setVisibility(View.GONE);
+         }
+    }
+
+    private void updateCurrentView() {
+        if(dataViewModel.isBackendDown().getValue()) {
+            currentView = ServicesView.BACKEND_ERROR;
+        }
+        else if(dataViewModel.getCurrentServices().getValue().isEmpty()){
+            currentView = ServicesView.NO_SERVICES;
+        }
+    }
+
+    private void initializeListView() {
         recyclerView.setLayoutManager(new LinearLayoutManager(requireContext(),
                                                               LinearLayoutManager.VERTICAL, false
         ));
-        Log.d("DEBUGGING", "setiando lista");
         ServiceFinderListAdapter adapter = new ServiceFinderListAdapter(
                 requireContext(),
                 dataViewModel.getCurrentServices().getValue()
         );
         recyclerView.setAdapter(adapter);
-
-        if(dataViewModel.getCurrentServices().getValue().isEmpty()) {
-            recyclerView.setVisibility(View.GONE);
-            noServicesOffered.setVisibility(View.VISIBLE);
-            noServicesAvailable.setVisibility(View.GONE);
-        } else {
-            recyclerView.setVisibility(View.VISIBLE);
-            noServicesOffered.setVisibility(View.GONE);
-            noServicesAvailable.setVisibility(View.GONE);
-        }
-
-        if(dataViewModel.isBackendDown().getValue()) {
-            recyclerView.setVisibility(View.GONE);
-            noServicesOffered.setVisibility(View.GONE);
-            noServicesAvailable.setVisibility(View.VISIBLE);
-        } else {
-            noServicesAvailable.setVisibility(View.GONE);
-        }
-
-        filterButton = view.findViewById(R.id.filters_button);
-        filterButton.setOnClickListener(this);
-
-        mapsButton = view.findViewById(R.id.maps_button);
-        mapsButton.setOnClickListener(this);
     }
 
     @Override
@@ -102,7 +140,8 @@ public class ServiceFinderFragment extends Fragment implements View.OnClickListe
                         .navigate(R.id.action_serviceFinderFragment_to_filtersFragment);
                 break;
             case R.id.maps_button:
-                Log.d("DEBUGGING", "maps");
+                currentView = ServicesView.MAP;
+                updateServicesView();
                 break;
         }
     }
