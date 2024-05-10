65c150e7459699e08dcec9e1b679d27ec224c8d3
tbotalla <tbotalla@gmail.com>
2024-05-09 21:17:10 -0300

    fiubyte/.github#42 & fiubyte/.github#43: Added logic for faving/unfaving a service

--- app/src/main/java/com/fiubyte/bafix/fragments/ServiceFragment.java
+++ app/src/main/java/com/fiubyte/bafix/fragments/ServiceFragment.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fiubyte.bafix.R;
 import com.fiubyte.bafix.entities.ProviderData;
@@ -22,17 +23,18 @@ import com.fiubyte.bafix.entities.ServiceData;
 import com.fiubyte.bafix.models.DataViewModel;
 import com.fiubyte.bafix.preferences.SharedPreferencesManager;
 import com.fiubyte.bafix.utils.ProvidersDataGenerator;
+import com.fiubyte.bafix.utils.ServicesListManager;
+import com.fiubyte.bafix.utils.ServicesListManager.ServiceRateCallback;
 import com.fiubyte.bafix.utils.SvgRatingBar;
 import com.google.android.material.card.MaterialCardView;
 import com.squareup.picasso.Picasso;
-
-import org.w3c.dom.Text;
 
 public class ServiceFragment extends Fragment implements View.OnClickListener {
     private DataViewModel dataViewModel;
     MaterialCardView providerCardView;
     ServiceData serviceData;
     ImageView backButton;
+    ImageView favoriteButton;
 
     SvgRatingBar ratingBar;
 
@@ -40,7 +42,7 @@ public class ServiceFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
-                            ) {
+    ) {
         return inflater.inflate(R.layout.fragment_service, container, false);
     }
 
@@ -50,26 +52,25 @@ public class ServiceFragment extends Fragment implements View.OnClickListener {
 
         dataViewModel = new ViewModelProvider(requireActivity()).get(DataViewModel.class);
 
-        if(ServiceFragmentArgs.fromBundle(getArguments()).getServiceData() != null){
+        if (ServiceFragmentArgs.fromBundle(getArguments()).getServiceData() != null) {
             serviceData = ServiceFragmentArgs.fromBundle(getArguments()).getServiceData();
         }
 
         ImageView providerPhoto = view.findViewById(R.id.provider_picture);
         Picasso.with(this.getContext()).load(serviceData.getServicePhotoURL()).resize(600, 600).centerCrop().into(providerPhoto);
 
-        ((TextView)view.findViewById(R.id.service_title)).setText(serviceData.getTitle());
-        ((TextView)view.findViewById(R.id.service_description)).setText(serviceData.getDescription());
-        ((TextView)view.findViewById(R.id.availability_days)).setText(serviceData.getAvailabilityDays());
-        ((TextView)view.findViewById(R.id.availability_time)).setText(serviceData.getAvailabilityTime());
-        ((TextView)view.findViewById(R.id.provider_name)).setText(serviceData.getProviderName());
-        ((TextView)view.findViewById(R.id.max_distance)).setText("A " + serviceData.getMaxDistance() + " km");
-        ((TextView)view.findViewById(R.id.provider_phone)).setText(serviceData.getProviderPhone());
+        ((TextView) view.findViewById(R.id.service_title)).setText(serviceData.getTitle());
+        ((TextView) view.findViewById(R.id.service_description)).setText(serviceData.getDescription());
+        ((TextView) view.findViewById(R.id.availability_days)).setText(serviceData.getAvailabilityDays());
+        ((TextView) view.findViewById(R.id.availability_time)).setText(serviceData.getAvailabilityTime());
+        ((TextView) view.findViewById(R.id.provider_name)).setText(serviceData.getProviderName());
+        ((TextView) view.findViewById(R.id.max_distance)).setText("A " + serviceData.getMaxDistance() + " km");
+        ((TextView) view.findViewById(R.id.provider_phone)).setText(serviceData.getProviderPhone());
 
         providerCardView = view.findViewById(R.id.provider_container);
         providerCardView.setOnClickListener(this);
 
         backButton = view.findViewById(R.id.back_button);
-
         backButton.setOnClickListener(v -> Navigation.findNavController(v).popBackStack());
 
         ratingBar = view.findViewById(R.id.rating_bar);
@@ -86,17 +87,56 @@ public class ServiceFragment extends Fragment implements View.OnClickListener {
                         .navigate(action);
             }
         });
+
+        favoriteButton = view.findViewById(R.id.favorite_button);
+        if (Boolean.TRUE.equals(dataViewModel.isServiceFaved().getValue())) {
+            favService();
+        }
+        favoriteButton.setOnClickListener(favButtonView -> {
+            String token = SharedPreferencesManager.getStoredToken(requireActivity());
+            if (Boolean.FALSE.equals(dataViewModel.isServiceFaved().getValue())) {
+                ServicesListManager.favService(token, serviceData.getServiceId(), new ServiceRateCallback() {
+                    @Override
+                    public void onSuccess(String response) {
+                        Log.d("DEBUGGING", "Fav service was successful");
+                        favService();
+                        showFavSuccess();
+                    }
+
+                    @Override
+                    public void onError() {
+                        Log.e("ERROR", "Fav service was unsuccessful");
+                        showFavError();
+                    }
+                });
+            } else {
+                ServicesListManager.unfavService(token, serviceData.getServiceId(), new ServiceRateCallback() {
+                    @Override
+                    public void onSuccess(String response) {
+                        Log.d("DEBUGGING", "Unfav service was successful");
+                        unfavService();
+                        showUnfavSuccess();
+                    }
+
+                    @Override
+                    public void onError() {
+                        Log.e("ERROR", "Unfav service was unsuccessful");
+                        showFavError();
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
         ProviderData providerData = new ProviderData(serviceData.getProviderId(),
-                                                     serviceData.getProviderName(),
-                                                     serviceData.getGeoPoint(),
-                                                     ProvidersDataGenerator.generateProviderServicesList(dataViewModel.getCurrentServices().getValue(), serviceData.getProviderId()),
-                                                     serviceData.getMaxDistance(),
-                                                     serviceData.getProviderPhotoURL(),
-                                                     serviceData.getProviderPhone()
+                serviceData.getProviderName(),
+                serviceData.getGeoPoint(),
+                ProvidersDataGenerator.generateProviderServicesList(dataViewModel.getCurrentServices().getValue(), serviceData.getProviderId()),
+                serviceData.getMaxDistance(),
+                serviceData.getProviderPhotoURL(),
+                serviceData.getProviderPhone()
 
         );
 
@@ -105,5 +145,36 @@ public class ServiceFragment extends Fragment implements View.OnClickListener {
         Navigation
                 .findNavController(view)
                 .navigate(action);
+    }
+
+    private void favService() {
+        favoriteButton.setImageResource(R.drawable.favorite);
+        dataViewModel.isServiceFaved().postValue(true);
+    }
+
+    private void unfavService() {
+        favoriteButton.setImageResource(R.drawable.not_favorite);
+        dataViewModel.isServiceFaved().postValue(false);
+    }
+
+    private void showFavError() {
+        getActivity().runOnUiThread(() -> Toast.makeText(getActivity(),
+                        "Ha ocurrido un error.\nPor favor, intenta de nuevo más tarde.",
+                        Toast.LENGTH_SHORT)
+                .show());
+    }
+
+    private void showFavSuccess() {
+        getActivity().runOnUiThread(() -> Toast.makeText(getActivity(),
+                        "¡Agregado a favoritos con éxito!",
+                        Toast.LENGTH_SHORT)
+                .show());
+    }
+
+    private void showUnfavSuccess() {
+        getActivity().runOnUiThread(() -> Toast.makeText(getActivity(),
+                        "¡Quitado de favoritos con éxito!",
+                        Toast.LENGTH_SHORT)
+                .show());
     }
 }
--- app/src/main/java/com/fiubyte/bafix/models/DataViewModel.java
+++ app/src/main/java/com/fiubyte/bafix/models/DataViewModel.java
@@ -14,8 +14,8 @@ public class DataViewModel extends ViewModel {
 
     private MutableLiveData<ArrayList<ServiceData>> services;
     private MutableLiveData<Map<String, Double>> location;
-
     private MutableLiveData<Boolean> isBackendDown;
+    private MutableLiveData<Boolean> serviceFaved;
 
     public MutableLiveData<ArrayList<ServiceData>> getCurrentServices() {
         if (services == null) {
@@ -36,6 +36,13 @@ public class DataViewModel extends ViewModel {
             isBackendDown = new MutableLiveData<>(false);
         }
         return isBackendDown;
+    }
+
+    public MutableLiveData<Boolean> isServiceFaved() {
+        if (serviceFaved == null) {
+            serviceFaved = new MutableLiveData<>(false);
+        }
+        return serviceFaved;
     }
 
     public void updateServices(ArrayList<ServiceData> services) {
